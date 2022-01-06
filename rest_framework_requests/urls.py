598b826a6aaaa52c36a204722904b6c53509ca64
suhailvs <suhailvs@gmail.com>
2022-01-06 17:26:36 +0530

    fixed django 4.0 bug

--- rest_framework_requests/urls.py
+++ rest_framework_requests/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework_requests.views import DRFDocsView
 
 urlpatterns = [
     # Url to view the API Docs
-    url(r'^$', DRFDocsView.as_view(), name='drfdocs'),
+    path('', DRFDocsView.as_view(), name='drfdocs'),
 ]
